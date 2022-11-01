33a85838787e438675476d41ae9cce4ee4e0647e
Akash More <akashkmore1@gmail.com>
2022-11-01 20:05:53 +0530

    Learn about byte

--- 01-Intrduction_To_Java/WholeNumbers.java
+++ 01-Intrduction_To_Java/WholeNumbers.java
@@ -1,9 +1,13 @@
 public class WholeNumbers {
     public static void main(String[] args) {
-        // int value occupies 8 bits of memory
+        // int value occupies 8 bytes of memory
         // So max and min value of int is ~ -2 billion to 2 billion
         int integer1 = 44;
         System.out.println(integer1);
 
+        // byte value occupies 1 byte of memory
+        // So max and min value of int is ~ -128 to 127
+        byte bytenum = 127;
+        System.out.println(bytenum);
     }
 }
