b1f51c51c933d0466bd9e06e6f2d6feab55382d5
Akash More <akashkmore1@gmail.com>
2022-11-01 20:08:56 +0530

    Learn about short

--- 01-Intrduction_To_Java/WholeNumbers.java
+++ 01-Intrduction_To_Java/WholeNumbers.java
@@ -1,6 +1,6 @@
 public class WholeNumbers {
     public static void main(String[] args) {
-        // int value occupies 8 bytes of memory
+        // int value occupies 4 bytes of memory
         // So max and min value of int is ~ -2 billion to 2 billion
         int integer1 = 44;
         System.out.println(integer1);
@@ -9,5 +9,10 @@ public class WholeNumbers {
         // So max and min value of int is ~ -128 to 127
         byte bytenum = 127;
         System.out.println(bytenum);
+
+        // short value occupies 2 byte of memory
+        // So max and min value of int is ~ -32,768 to 32,767
+        short shortNum = 30000;
+        System.out.println(shortNum);
     }
 }
