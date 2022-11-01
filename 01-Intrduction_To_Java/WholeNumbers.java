a20556b68e1e72176e449baafff5582a22bcdf1c
Akash More <akashkmore1@gmail.com>
2022-11-01 20:02:17 +0530

    Learn about int datatype

---
+++ 01-Intrduction_To_Java/WholeNumbers.java
@@ -0,0 +1,9 @@
+public class WholeNumbers {
+    public static void main(String[] args) {
+        // int value occupies 8 bits of memory
+        // So max and min value of int is ~ -2 billion to 2 billion
+        int integer1 = 44;
+        System.out.println(integer1);
+
+    }
+}
