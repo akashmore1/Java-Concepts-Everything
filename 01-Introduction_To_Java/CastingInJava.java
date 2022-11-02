0c6f1d6f60761fa02cc300a7ce15bef3a849de81
Akash More <akashkmore1@gmail.com>
2022-11-02 21:35:47 +0530

    Learn casting in java

--- 01-Introduction_To_Java/CastingInJava.java
+++ 01-Introduction_To_Java/CastingInJava.java
@@ -1,5 +1,17 @@
 public class CastingInJava {
     public static void main(String[] args) {
-        System.out.println("Casting");
+
+        byte num = 44;
+
+        // here in java expression by default returns an integer.
+        // If we want to store value in byte or something other than int, we need to
+        // cast it
+        byte half = (byte) (num / 2);
+        System.out.println(half);
+
+        int halfInt = num / 2;
+        System.out.println(halfInt);
+
+        float halfDoubleType = (float) (num / 2);
     }
 }
