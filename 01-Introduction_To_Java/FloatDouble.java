6a6014a8bc6a67234006287744c36c776e3d82c2
Akash More <akashkmore1@gmail.com>
2022-11-03 22:35:48 +0530

    Learn about float and double

---
+++ 01-Introduction_To_Java/FloatDouble.java
@@ -0,0 +1,16 @@
+// Double is said to be default type in float and double
+
+public class FloatDouble {
+    public static void main(String[] args) {
+        float floatingNumber = 44.22f;
+        // In above case if we don't specify 'f' at the end, we get an error
+        System.out.println(floatingNumber);
+
+        double doubleTypeNumber = 77.432;
+        /*
+         * In above case, we don't need to specify 'd' at the end, as default value is
+         * double
+         */
+        System.out.println(doubleTypeNumber);
+    }
+}
