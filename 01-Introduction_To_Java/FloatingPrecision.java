41453a5931985da1345c2882c4588a1bbd24f3cd
Akash More <akashkmore1@gmail.com>
2022-11-04 23:55:45 +0530

    Note about double

--- 01-Introduction_To_Java/FloatingPrecision.java
+++ 01-Introduction_To_Java/FloatingPrecision.java
@@ -5,5 +5,10 @@ public class FloatingPrecision {
 
         float floatNum = 7f / 2f;
         System.out.println(floatNum); // retun 3.5
+
+        double doubleTypeNumber = (7d / 2d); // Need to specify d here as nums will be considered int
+        System.out.println(doubleTypeNumber);
+
+        // Let's see precision in float and double value
     }
 }
