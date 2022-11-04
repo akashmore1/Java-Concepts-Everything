88e39debe05577bfd6252e37306575905965731f
Akash More <akashkmore1@gmail.com>
2022-11-04 23:45:22 +0530

    Range of float and double

--- 01-Introduction_To_Java/FloatDouble.java
+++ 01-Introduction_To_Java/FloatDouble.java
@@ -2,10 +2,12 @@
 
 public class FloatDouble {
     public static void main(String[] args) {
+        // float have 4 byte of memory
         float floatingNumber = 44.22f;
         // In above case if we don't specify 'f' at the end, we get an error
         System.out.println(floatingNumber);
 
+        // double have 8 byte of memory
         double doubleTypeNumber = 77.432;
         /*
          * In above case, we don't need to specify 'd' at the end, as default value is
