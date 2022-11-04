8790125e4adb6e0f491b3f92d4020eedc6449ee1
Akash More <akashkmore1@gmail.com>
2022-11-04 23:49:09 +0530

    Rounding in int type

---
+++ 01-Introduction_To_Java/FloatingPrecision.java
@@ -0,0 +1,9 @@
+public class FloatingPrecision {
+    public static void main(String[] args) {
+        int intNum = 7 / 2;
+        System.out.println(intNum); // return 3
+
+        float floatNum = 7f / 2f;
+        System.out.println(floatNum); // retun 3.5
+    }
+}
