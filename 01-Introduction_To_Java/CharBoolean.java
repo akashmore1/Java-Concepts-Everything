98a1db62f88295a3795c7646eea0a9be2b2723de
Akash More <akashkmore1@gmail.com>
2022-11-05 21:01:17 +0530

    Learn about char

---
+++ 01-Introduction_To_Java/CharBoolean.java
@@ -0,0 +1,9 @@
+public class CharBoolean {
+    public static void main(String[] args) {
+        // Char can only have 1 character
+        // Char occupies 2 bytes of memory
+        // we acn save unicode in char as well
+        char firstLetter = 'A';
+        System.out.println(firstLetter);
+    }
+}
