770a3bdd993a8af3793acf69728b0479b89c9cae
Akash More <akashkmore1@gmail.com>
2022-11-06 21:17:22 +0530

    Learn about strings

--- 01-Introduction_To_Java/PrimitiveDatatypesRecap.java
+++ 01-Introduction_To_Java/PrimitiveDatatypesRecap.java
@@ -1,3 +1,22 @@
 public class PrimitiveDatatypesRecap {
+    public static void main(String[] args) {
+        // string is a datatype in java which is not primitive type
+        // It's actually a class
+        String myString = "This is a a string";
+        System.out.println(myString);
 
+        myString = myString + " and this is more";
+        System.out.println(myString);
+
+        // Just like js, when we add stringNumber to a number, we get concatenation
+        int num = 42;
+        String numStr = "77";
+        numStr = num + numStr;
+        System.out.println(numStr); // gives 4277
+
+        // We can delete characters from string but
+        // that's not completely true
+        // Strings in java are immutable
+        // When we make changes in a string, we are actually creating a new string
+    }
 }
