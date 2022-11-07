740c4e5e681f63229c66e31473dece04bbf19992
Akash More <akashkmore1@gmail.com>
2022-11-07 23:51:00 +0530

    call by value call by reference

--- 01-Introduction_To_Java/OperatorsOperand.java
+++ 01-Introduction_To_Java/OperatorsOperand.java
@@ -2,6 +2,7 @@ public class OperatorsOperand {
     public static void main(String[] args) {
         int result = 1 + 2;
         System.out.println(result); // gives 3
-
+        // primitive datatypes are call by value type(Js concerpt of call by value and
+        // call by reference)
     }
 }
