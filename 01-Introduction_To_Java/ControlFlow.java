a620ef86384367a55a62f382db8ef2f6fd1a0d3f
Akash More <akashkmore1@gmail.com>
2022-11-08 22:44:33 +0530

    Start learning if else

---
+++ 01-Introduction_To_Java/ControlFlow.java
@@ -0,0 +1,10 @@
+public class ControlFlow {
+    public static void main(String[] args) {
+        int number = 4321;
+        if (number == 4322) {
+            System.out.println("Condition true");
+        } else {
+            System.out.println("Condition false");
+        }
+    }
+}
