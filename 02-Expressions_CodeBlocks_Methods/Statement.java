9a1b760874a2ecb26cbffd1dce2aa0ca4d3c73a9
Akash More <akashkmore1@gmail.com>
2022-11-10 23:27:59 +0530

    Learn about statements

---
+++ 02-Expressions_CodeBlocks_Methods/Statement.java
@@ -0,0 +1,5 @@
+public class Statement {
+    // We can write all java code in 1 line.
+    // It is highly recommended to have proper identations
+    // But java is not indent language like python
+}
