e8ebbba7b05f17ee6004d5813a644995b4cb11b3
Akash More <akashkmore1@gmail.com>
2022-11-10 23:50:03 +0530

    Methods in java

---
+++ 02-Expressions_CodeBlocks_Methods/Methods.java
@@ -0,0 +1,15 @@
+public class Methods {
+    public static void main(String[] args) {
+        int finalScore1 = calculateScore(true, 100, 44);
+        int finalScore2 = calculateScore(true, 10, 470);
+        System.out.println(finalScore1 + " " + finalScore2);
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int level) {
+        if (gameOver) {
+            int finalScore = score * level;
+            return finalScore;
+        }
+        return 0;
+    }
+}
