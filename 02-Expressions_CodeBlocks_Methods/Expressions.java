f87b716c06be13ef56d0655b72363ede4fdc20e3
Akash More <akashkmore1@gmail.com>
2022-11-10 23:23:15 +0530

    Start expressions

---
+++ 02-Expressions_CodeBlocks_Methods/Expressions.java
@@ -0,0 +1,7 @@
+public class Expressions {
+    public static void main(String[] args) {
+        // left of '=' is an expression
+        double kiloMeters = 100d * 20.2447d;
+        System.out.println(kiloMeters);
+    }
+}
