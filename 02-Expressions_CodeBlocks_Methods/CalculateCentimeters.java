d87c0da364619a60c7b7b2b1d48582ced6b93c76
Akash More <akashkmore1@gmail.com>
2022-11-14 23:05:35 +0530

    Notes of method overloading

--- 02-Expressions_CodeBlocks_Methods/CalculateCentimeters.java
+++ 02-Expressions_CodeBlocks_Methods/CalculateCentimeters.java
@@ -1,7 +1,7 @@
 public class CalculateCentimeters {
     // Here we are using method overloading with different number of parameters.
     // Method overloading cannot be used only by changing return type
-
+    // In fact we should not use method overloading, if return type is different
     public static void main(String[] args) {
         System.out.println(calcFeetAndInchesToCentimeters(2, 7));
         System.out.println(calcFeetAndInchesToCentimeters(4));
