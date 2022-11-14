e1c2fccc86bed0df842892b62d99d5a0f808b9e7
Akash More <akashkmore1@gmail.com>
2022-11-14 23:01:52 +0530

    Calculate cms

---
+++ 02-Expressions_CodeBlocks_Methods/CalculateCentimeters.java
@@ -0,0 +1,22 @@
+public class CalculateCentimeters {
+    public static void main(String[] args) {
+        System.out.println(calcFeetAndInchesToCentimeters(2, 7));
+        System.out.println(calcFeetAndInchesToCentimeters(4));
+    }
+
+    public static double calcFeetAndInchesToCentimeters(int feet, int inches) {
+        if (feet < 0 || inches < 0 || inches > 12) {
+            return -1;
+        }
+        double cmValue = (feet * 30d + inches * 2.54d);
+        return cmValue;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(int inches) {
+        if (inches < 0 || inches > 12) {
+            return -1;
+        }
+        double cmValue = inches * 2.54d;
+        return cmValue;
+    }
+}
