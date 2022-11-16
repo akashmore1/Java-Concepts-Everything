306507593d515754658b2210d304519f7201b1d9
Akash More <akashkmore1@gmail.com>
2022-11-16 21:39:34 +0530

    Learn switch statement

---
+++ 03-Contol_Flow_Statements/SwitchStatement.java
@@ -0,0 +1,44 @@
+public class SwitchStatement {
+
+    public static void main(String[] args) {
+        printDayOfTheWeek(8);
+    }
+
+    public static void printDayOfTheWeek(int day) {
+        // 0 = sunday
+        // 1 = monday etc
+        switch (day) {
+            case 0:
+                System.out.println("Sunday");
+                break;
+
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            case 4:
+                System.out.println("Thursday");
+                break;
+
+            case 5:
+                System.out.println("Friday");
+                break;
+
+            case 6:
+                System.out.println("Saturday");
+                break;
+
+            default:
+                System.out.println("Invalid Day");
+                break;
+        }
+    }
+}
