6dbe8897912b93b5b8cf7512bcb036ff4f85c05b
Akash More <akashkmore1@gmail.com>
2022-11-19 20:28:07 +0530

    Input string using scanner

--- 03-Contol_Flow_Statements/Input.java
+++ 03-Contol_Flow_Statements/Input.java
@@ -11,6 +11,11 @@ public class Input {
     }
 
     public static void main(String[] args) {
-        calcAge();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String userName = scanner.nextLine();
+        int age = calcAge();
+
+        System.out.println(userName + " your age is " + age);
     }
 }
