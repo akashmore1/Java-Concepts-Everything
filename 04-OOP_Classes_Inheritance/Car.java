dc0f0328866f4da8a7472c76246ff9f23e12aade
Akash More <akashkmore1@gmail.com>
2022-11-20 21:15:25 +0530

    Create first class Car

---
+++ 04-OOP_Classes_Inheritance/Car.java
@@ -0,0 +1,6 @@
+public class Car {
+    private String colour;
+    private int wheels;
+    private int doors;
+    private String model;
+}
--- 04-OOP_Classes_Inheritance/Classes.java
+++ 04-OOP_Classes_Inheritance/Classes.java
@@ -1,4 +1,4 @@
 public class Classes {
     // Classes are fundamental component of Object oriented programming
-
+    // We can call classes user defined datatypes.
 }
