b12c953a414e33bd27e43526dcda4b080ff3b38a
Akash More <akashkmore1@gmail.com>
2022-11-20 21:20:51 +0530

    Learn about setting values in class through variables

--- 04-OOP_Classes_Inheritance/Car.java
+++ 04-OOP_Classes_Inheritance/Car.java
@@ -5,4 +5,20 @@ public class Car {
     private int wheels;
     private int doors;
     private String model;
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
 }
