40f26b5f973133798358408661696c35d08ad5f4
Akash More <akashkmore1@gmail.com>
2022-11-21 21:23:30 +0530

    Create person class and add some methods

---
+++ 04-OOP_Classes_Inheritance/person/Person.java
@@ -0,0 +1,56 @@
+package person;
+
+public class Person {
+    // write your code here
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setFirstName(String fname) {
+        this.firstName = fname;
+    }
+
+    public void setLastName(String lname) {
+        this.lastName = lname;
+    }
+
+    public void setAge(int age) {
+        if (age >= 0 && age <= 100) {
+            this.age = age;
+        } else {
+            this.age = 0;
+        }
+    }
+
+    public boolean isTeen() {
+        if (this.age > 12 && this.age < 20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getFullName() {
+        if (firstName.isEmpty() && lastName.isEmpty()) {
+            return "";
+        } else if (firstName.isEmpty()) {
+            return this.lastName;
+        } else if (lastName.isEmpty()) {
+            return this.firstName;
+        } else {
+            return this.firstName + " " + this.lastName;
+        }
+    }
+}
