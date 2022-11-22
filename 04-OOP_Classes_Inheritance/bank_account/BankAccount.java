ad78d321fc29763cc39038f6779ca6711288a9e0
Akash More <akashkmore1@gmail.com>
2022-11-22 23:24:42 +0530

    Add methods to bank account

--- 04-OOP_Classes_Inheritance/bank_account/BankAccount.java
+++ 04-OOP_Classes_Inheritance/bank_account/BankAccount.java
@@ -2,9 +2,48 @@ package bank_account;
 
 public class BankAccount {
     private long accountNumber;
-    private double balance;
+    private double balance = 0;
     private String customerName;
     private String email;
     private long phoneNumber;
 
+    public void setAccountNumber(long accNum) {
+        this.accountNumber = accNum;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setCustomerName(String name) {
+        this.customerName = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void depositFunds(double funds) {
+        this.balance += funds;
+        System.out.println(this.balance);
+    }
+
+    public void withdrawFunds(double funds) {
+
+        if (funds > this.balance) {
+            System.out.println("Insufficient balance");
+        } else {
+            this.balance -= funds;
+            System.out.println(this.balance);
+        }
+    }
+
+    // public void printAccountDetail(){
+    // System.out.println("Name: " + this.customerName);
+    // System.out.println();
+    // }
 }
---
+++ 04-OOP_Classes_Inheritance/bank_account/Main.java
@@ -0,0 +1,6 @@
+package bank_account;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
