55dfd9e1608e7e3ab6fd436635eb824af06b7b4c
Akash More <akashkmore1@gmail.com>
2022-11-22 22:59:46 +0530

    Create bank account class and its fields

---
+++ 04-OOP_Classes_Inheritance/bank_account/BankAccount.java
@@ -0,0 +1,10 @@
+package bank_account;
+
+public class BankAccount {
+    private long accountNumber;
+    private double balance;
+    private String customerName;
+    private String email;
+    private long phoneNumber;
+
+}
