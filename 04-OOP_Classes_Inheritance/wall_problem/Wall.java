a1cedb017ef0aae2272b8ff84d9d24eecd949d72
Akash More <akashkmore1@gmail.com>
2022-11-23 23:32:41 +0530

    Learn constructor function in detail

--- 04-OOP_Classes_Inheritance/wall_problem/Wall.java
+++ 04-OOP_Classes_Inheritance/wall_problem/Wall.java
@@ -2,4 +2,7 @@ package wall_problem;
 
 public class Wall {
     // Constructor is a function gets executed when instance of class is created.
+    // Constructor function is named exactly same as class name
+    // Constructor function can be overloaded provided we are giving different
+    // parameters to function
 }
