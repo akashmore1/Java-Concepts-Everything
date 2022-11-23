16c789e6812082933a63c8cabac45fe9937ef686
Akash More <akashkmore1@gmail.com>
2022-11-23 23:43:45 +0530

    Create constructor function and solve Wall area problem

--- 04-OOP_Classes_Inheritance/wall_problem/Wall.java
+++ 04-OOP_Classes_Inheritance/wall_problem/Wall.java
@@ -1,8 +1,56 @@
 package wall_problem;
 
 public class Wall {
+    private double width;
+    private double height;
+
     // Constructor is a function gets executed when instance of class is created.
     // Constructor function is named exactly same as class name
     // Constructor function can be overloaded provided we are giving different
     // parameters to function
+
+    public Wall() {
+    }
+
+    public Wall(double width, double height) {
+        if (width < 0) {
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+
+        if (height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public void setWidth(double width) {
+        if (width < 0) {
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+    }
+
+    public void setHeight(double height) {
+        if (height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
+    public double getArea() {
+        return this.width * this.height;
+    }
 }
