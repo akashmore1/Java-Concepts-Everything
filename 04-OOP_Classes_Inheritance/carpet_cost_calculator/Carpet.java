7ac8c0560586c6e5b64febc4e39a4014b2ec2e9d
Akash More <akashkmore1@gmail.com>
2022-11-24 23:16:21 +0530

    Create class carpet and calculate cost

--- 04-OOP_Classes_Inheritance/carpet_cost_calculator/Carpet.java
+++ 04-OOP_Classes_Inheritance/carpet_cost_calculator/Carpet.java
@@ -1,5 +1,17 @@
 package carpet_cost_calculator;
 
 public class Carpet {
+    private double cost;
 
+    public Carpet(double cost) {
+        if (cost < 0) {
+            this.cost = 0;
+        } else {
+            this.cost = cost;
+        }
+    }
+
+    public double getCost() {
+        return this.cost;
+    }
 }
