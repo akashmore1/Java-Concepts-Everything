61a951661bc6e79c98029e8a67e9245c2b80f830
Akash More <akashkmore1@gmail.com>
2022-11-24 22:49:40 +0530

    Calculate distance of point from origin

---
+++ 04-OOP_Classes_Inheritance/point_distance/Point.java
@@ -0,0 +1,42 @@
+package point_distance;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point() {
+
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance() {
+        double distance = Math.sqrt(this.x * this.x + this.y * this.y);
+        return distance;
+    }
+
+    public double distance(int x, int y) {
+        double distance = Math.sqrt(x * x + y * y);
+        return distance;
+    }
+
+}
