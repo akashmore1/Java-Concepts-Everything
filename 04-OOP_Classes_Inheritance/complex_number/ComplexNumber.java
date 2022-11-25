dab5f0b6c77f2484e702c18464167eba0931e7ca
Akash More <akashkmore1@gmail.com>
2022-11-25 23:02:49 +0530

    Add operations on complex number

--- 04-OOP_Classes_Inheritance/complex_number/ComplexNumber.java
+++ 04-OOP_Classes_Inheritance/complex_number/ComplexNumber.java
@@ -17,4 +17,21 @@ public class ComplexNumber {
         return this.imaginary;
     }
 
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void add(ComplexNumber complexNumber) {
+        add(complexNumber.getReal(), complexNumber.getImaginary());
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber complexNumber) {
+        subtract(complexNumber.getReal(), complexNumber.getImaginary());
+    }
 }
