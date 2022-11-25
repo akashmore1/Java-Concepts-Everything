9f510a23e466fd128c15ad54e3aaa074b05bf328
Akash More <akashkmore1@gmail.com>
2022-11-25 23:18:28 +0530

    Add getter and setter to Animal class

--- 04-OOP_Classes_Inheritance/inheritance_I/Animal.java
+++ 04-OOP_Classes_Inheritance/inheritance_I/Animal.java
@@ -1,5 +1,50 @@
 package inheritance_I;
 
 public class Animal {
+    private String name;
+    private int brain;
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBrain(int brain) {
+        this.brain = brain;
+    }
+
+    public void setBody(int body) {
+        this.body = body;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    private int body;
+    private int size;
+    private int weight;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBrain() {
+        return brain;
+    }
+
+    public int getBody() {
+        return body;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
 }
