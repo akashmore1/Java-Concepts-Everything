ff5392ae6fa5b6a71d2b84b01c4af70e8dc01005
Akash More <akashkmore1@gmail.com>
2022-11-26 23:13:01 +0530

    Palindrome problem

---
+++ 04-OOP_Classes_Inheritance/palindrome_string/PalindromeString.java
@@ -0,0 +1,10 @@
+package palindrome_string;
+
+public class PalindromeString {
+    public boolean isPalindrome(String string) {
+        int start = 0;
+        int end = string.length() - 1;
+
+        return true;
+    }
+}
