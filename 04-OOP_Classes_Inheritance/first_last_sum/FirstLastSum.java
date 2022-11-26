5333d85a9a7de54d3d404a43b2dda8f124a5095a
Akash More <akashkmore1@gmail.com>
2022-11-26 23:35:01 +0530

    Start solving sumFirstAndLastDigit problem

---
+++ 04-OOP_Classes_Inheritance/first_last_sum/FirstLastSum.java
@@ -0,0 +1,20 @@
+package first_last_sum;
+
+public class FirstLastSum {
+    public static int sumFirstAndLastDigit(int num) {
+        // if (num < 0) {
+        // return -1;
+        // }
+        // int lastInt = num % 10;
+        // int firstInt = 0;
+
+        // // To find the first digit of the number
+        // while (num != 0) {
+        // firstInt = Math.abs(num % 10);
+        // num /= 10;
+        // }
+
+        // return lastInt + firstInt;
+        return -1;
+    }
+}
