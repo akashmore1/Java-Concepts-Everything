364a1aacbcd3fd62bebb47fb4213371061fbfd09
Akash More <akashkmore1@gmail.com>
2022-11-30 23:11:26 +0530

    Solve perfect number problem

---
+++ 04-OOP_Classes_Inheritance/perfect_number/Main.java
@@ -0,0 +1,8 @@
+package perfect_number;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean a = PerfectNumber.isPerfectNumber(4);
+        System.out.println(a);
+    }
+}
