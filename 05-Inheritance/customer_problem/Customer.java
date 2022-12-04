c2d8c5ced21a2a289f5f01d02a37b380f13d205b
Akash More <akashkmore1@gmail.com>
2022-12-04 23:53:07 +0530

    Create customer class

---
+++ 05-Inheritance/customer_problem/Customer.java
@@ -0,0 +1,25 @@
+
+public class Customer {
+    private String name;
+    private double creditLimit;
+    private String emailAddress;
+
+    public Customer(String name, double creditLimit, String email) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.emailAddress = email;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getCreditLimit() {
+        return this.creditLimit;
+    }
+
+    public String getEmailAddress() {
+        return this.emailAddress;
+    }
+
+}
