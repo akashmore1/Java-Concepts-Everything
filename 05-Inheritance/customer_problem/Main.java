55c17b0f94b584d84d6c7073df1f1437767e66a9
Akash More <akashkmore1@gmail.com>
2022-12-07 23:51:48 +0530

    Start learning oop

--- 05-Inheritance/customer_problem/Main.java
+++ 05-Inheritance/customer_problem/Main.java
@@ -2,6 +2,7 @@ package customer_problem;
 
 public class Main {
     public static void main(String[] args) {
+        // Creating new object
         Customer customer = new Customer("Akash", 9999, "akash@gmail.com");
     }
 }
