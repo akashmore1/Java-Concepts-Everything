3e75aa9b59b630275d9a1949c28d30a8e0ef1228
Akash More <akashkmore1@gmail.com>
2022-12-09 23:17:10 +0530

    Learn about objects

--- 05-Inheritance/customer_problem/Main.java
+++ 05-Inheritance/customer_problem/Main.java
@@ -3,6 +3,7 @@ package customer_problem;
 public class Main {
     public static void main(String[] args) {
         // Creating new object
+        // Java objects are reference type
         Customer customer = new Customer("Akash", 9999, "akash@gmail.com");
 
         customer.getName();
