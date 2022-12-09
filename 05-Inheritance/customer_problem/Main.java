c74f55c36b97b13e2dcd858ba76f4a1c93fc80a0
Akash More <akashkmore1@gmail.com>
2022-12-09 23:18:56 +0530

    Learn about static variables

--- 05-Inheritance/customer_problem/Main.java
+++ 05-Inheritance/customer_problem/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         // Creating new object
         // Java objects are reference type
+        // When we change value of a static variable, its value changes for every
+        // object.
         Customer customer = new Customer("Akash", 9999, "akash@gmail.com");
 
         customer.getName();
